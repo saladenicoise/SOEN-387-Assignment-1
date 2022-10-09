1ac9bc8a7c20a3a41a081622aeb9a949a8b7573b
Jules Petit <jules.laptop@gmail.com>
2022-10-09 12:04:08 -0400

    Report generation done

--- config/config.php
+++ config/config.php
@@ -15,6 +15,7 @@ const PATH_REG_USER_ACCOUNT = '../../presentation/login/registerUserAccount.php'
 const PATH_MAIN_PAGE = '../../presentation/mainPage/mainPage.php';
 const PATH_INDEX = '../../index.html';
 const PATH_ADD_COURSE = '../../presentation/courses/addCourse.php';
+const PATH_REPORT_UTILS = '../../business/util/reportUtils.php';
 
 // Currently Unused File Paths
 //const PATH_PRINT_STAT = '../../business/js/printStat.js';
---
+++ data/reportGeneration/studentToCoursesScript.php
@@ -0,0 +1,47 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once('../../config/config.php');
+    require_once('../../business/login/passwordUtils.php');
+
+    $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $date = date('d-m-y-s');
+    $file = "report_courseList". $date .".txt";
+    $txt = fopen($file, "w") or die("Unable to generate report file!");
+
+    $id = test_input($_POST["sid"]);
+
+    fwrite($txt, "Student with ID " . $id . " is registered for the following courses:\n");
+    $SQL = $conn->prepare("SELECT course_code, semester FROM `registrar` WHERE id=?");
+    $SQL->bind_param("s", $id);
+    $SQL->execute();
+    $result = $SQL->get_result(); //mysqli_result object
+    $SQL->close();
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) { //For every course
+        $SQL = $conn->prepare("SELECT * FROM `course` WHERE course_code=? AND semester=?");
+        $SQL->bind_param("ss", $row["course_code"], $row["semester"]);
+        $SQL->execute();
+        $courseResult = $SQL->get_result();
+        $course = $courseResult->fetch_array(MYSQLI_ASSOC);
+        fwrite($txt, "Course Code: " . $course["course_code"] . " | Title: " . $course["title"] . " | Semester: " . $course["semester"] . " | Days: " . $course["days"] . " | Time: " . $course["time"] . " | Instructor: " . $course["instructor"] . " | Room: " . $course["room"] . " | Start Date: " . $course["start_date"] . " | End Date: " . $course["end_date"] . "\n");
+        $SQL->close();
+    }
+    fclose($txt);
+    $conn->close();
+    // Downloads Report File
+    header('Content-Description: File Transfer');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    header("Content-Type: text/plain");
+    readfile($file);
+    unlink("report_courseList". $date .".txt");
+}
+
+?>
